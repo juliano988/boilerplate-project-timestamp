256238d4802feeeb91c0fad1a79f46a58a42a2f6
juliano988 <julio-pinheiroo@hotmail.com>
2020-11-20 20:05:46 -0300

    20/11/2020
    Updates from of first day of project.

--- server.js
+++ server.js
@@ -4,6 +4,15 @@
 // init project
 var express = require('express');
 var app = express();
+const bodyParser = require('body-parser')
+const multer = require('multer') // v1.0.5
+const upload = multer() // for parsing multipart/form-data
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+// To manage the .env file:
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -18,13 +27,14 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.post("/api/timestamp/selected-date",function(req,res){
+  res.json({unix: Date.parse(req.body.date), utc: Date(req.body.date)});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
